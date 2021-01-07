3ff218ed0254b72803600bcebc77580c930a2a48
Nate Marti <nate_marti@sil.org>
2021-01-07 11:29:26 +0100

    use 2 spaces for line endings

--- python/scrape-site.py
+++ python/scrape-site.py
@@ -33,7 +33,6 @@ class Parse(HTMLParser):
 
     def handle_data(self, data):
         pass
-        #self.datas.append(data)
 
 
 def getResponse(url):
@@ -57,4 +56,4 @@ p = Parse()
 p.feed(data)
 
 for l in p.links:
-    print(f"[{l['title']}]({l['url']})\n")
+    print(f"[{l['title']}]({l['url']})  ")
