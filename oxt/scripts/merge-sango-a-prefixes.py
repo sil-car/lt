5a427ad96db619bda2776bb59d52b51ecc722a94
Nate Marti <nate_marti@sil.org>
2023-02-17 06:28:35 +0100

    don't add A flag more than once

--- oxt/scripts/merge-sango-a-prefixes.py
+++ oxt/scripts/merge-sango-a-prefixes.py
@@ -20,15 +20,20 @@ with infile.open() as f:
 words = [l.strip() for l in infile_lines]
 words = words[1:] # remove first line word count
 possible_plurals = [w for w in words if w[0] == 'a' or w[0] == 'â' or w[:2] == 'a-' or w[:2] == 'â-']
+possible_plurals = [p for p in possible_plurals if len(p) > 1] # remove single-letter items
 
 updated_words = words.copy()
 plurals = []
 for p in possible_plurals:
     for i, w in enumerate(words):
-        if len(p) < 2:
+        if len(w) < 2:
+            # Skip single-letter items.
             continue
         if p[1:] == w or (p[1] == '-' and p[2:] == w):
             # print(f"Found singular \"{words[i]}\" for {p}")
+            if 'A' in updated_words[i].split('/')[1]:
+                # Don't add 'A' more than once.
+                continue
             updated_words[i] = f"{updated_words[i]}/A"
             plurals.append(p)
 
