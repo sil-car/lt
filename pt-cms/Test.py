16e9e59dcc92a75e7233959ede99b3e24e673953
Nate Marti <nate_marti@sil.org>
2023-09-11 18:28:52 +0100

    add PT CMS Test files

---
+++ pt-cms/Test.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.stderr.write(__name__ + '\n')
+sys.stderr.write(Project + '\n')
+sys.stderr.write(str(dir()) + '\n')
+# sys.stderr.write(str(globals()) + '\n')
+sys.stderr.write(SettingsDirectory + '\n')
+
