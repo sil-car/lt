bec85af919c638117228f9328bcaf11e83ede811
Nate Marti <nate_marti@sil.org>
2024-03-27 17:43:00 +0100

    LfL updates

--- web/lfl-json2odt.py
+++ web/lfl-json2odt.py
@@ -13,19 +13,25 @@ from pathlib import Path
 lfl_api = "https://luke.silcameroon.org/api"
 lfl_data = Path(__file__).resolve().parent / 'lfl-data'
 lfl_data.mkdir(exist_ok=True)
+outfile = lfl_data / 'LessonsFromLuke_sg.odt'
 # languages = [2, 5]
-language = [5] # Sango JSON files include both "source" (FR) and "text" (SG)
+languages = [5] # Sango JSON files include both "source" (FR) and "text" (SG)
 lessons = range(1, 113)
 
-# Copy JSON files to lfl_data.
-for language in languages:
-    for lesson in lessons:
-        outfile = lfl_data / f"{language}-{lesson}-tStrings.json"
-        print(f"Downloading language {language}, lesson {lesson}...")
-        if not outfile.is_file():
+ans = input("Download text from the website? [y/N]: ")
+if ans and ans.lower()[0] == 'y':
+    # Copy JSON files to lfl_data.
+    for language in languages:
+        for lesson in lessons:
+            outf = lfl_data / f"{language}-{lesson}-tStrings.json"
+            print(f"Downloading language {language}, lesson {lesson}...")
             try:
                 r = requests.get(f"{lfl_api}/languages/5/lessons/1/tStrings")
             except KeyboardInterrupt:
                 print("\nInterrupted with Ctrl-C.")
                 exit(1)
-            outfile.write_text(r.text)
+            outf.write_text(r.text)
+
+data_files = [f for f in lfl_data.glob('*.json')]
+data_files.sort()
+
